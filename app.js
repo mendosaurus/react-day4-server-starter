b08eaf0b6d83453b04476aa6332ffe57343888cb
mendosaurus <masaki.endo@nex.nikkei.com>
2019-12-06 11:50:57 -0500

    define models,build a REST API to provide access to the resources

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const bodyParser   = require('body-parser');
-const cookieParser = require('cookie-parser');
-const express      = require('express');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
-const mongoose     = require('mongoose');
-const logger       = require('morgan');
-const path         = require('path');
-
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const favicon = require("serve-favicon");
+const hbs = require("hbs");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const path = require("path");
+const cors = require("cors");
 
 // WHEN INTRODUCING USERS DO THIS:
 // INSTALL THESE DEPENDENCIES: passport-local, passport, bcryptjs, express-session
@@ -22,57 +22,66 @@ const path         = require('path');
 // IF YOU STILL DIDN'T, GO TO 'configs/passport.js' AND UN-COMMENT OUT THE WHOLE FILE
 
 mongoose
-  .connect('mongodb://localhost/project-management-server', {useNewUrlParser: true})
+  .connect("mongodb://localhost/project-management-server", {
+    useNewUrlParser: true
+  })
   .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
   })
   .catch(err => {
-    console.error('Error connecting to mongo', err)
+    console.error("Error connecting to mongo", err);
   });
 
-const app_name = require('./package.json').name;
-const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+const app_name = require("./package.json").name;
+const debug = require("debug")(
+  `${app_name}:${path.basename(__filename).split(".")[0]}`
+);
 
 const app = express();
 
 // Middleware Setup
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Express View engine setup
 
-app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
-}));
-      
+app.use(
+  require("node-sass-middleware")({
+    src: path.join(__dirname, "public"),
+    dest: path.join(__dirname, "public"),
+    sourceMap: true
+  })
+);
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
 
 // ADD SESSION SETTINGS HERE:
 
-
 // USE passport.initialize() and passport.session() HERE:
 
-
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
-
+app.locals.title = "Express - Generated with IronGenerator";
 
 // ADD CORS SETTINGS HERE TO ALLOW CROSS-ORIGIN INTERACTION:
-
-
+app.use(
+  cors({
+    credentials: true, //credentials will come to play when we introduce users and origin points to the allowed url-s, in our case that is React’s server that is running on port 3000
+    origin: ["http://localhost:3000"] // <== this will be the URL of our React app (it will be running on port 3000. Origin is the array so that gives us opportunity to add as many URLs as we need.)
+  })
+);
 
 // ROUTES MIDDLEWARE STARTS HERE:
-
-const index = require('./routes/index');
-app.use('/', index);
-
+app.use("/api", require("./routes/project-routes"));
+app.use("/api", require("./routes/task-routes"));
+app.use("/api", require("./routes/task-routes"));
+const index = require("./routes/index");
+app.use("/", index);
 
 module.exports = app;
