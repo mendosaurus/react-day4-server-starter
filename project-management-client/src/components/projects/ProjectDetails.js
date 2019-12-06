f98cf7278bf766edd0635354e0aaf975ac685f8f
mendosaurus <masaki.endo@nex.nikkei.com>
2019-12-06 16:13:22 -0500

    make all the components we need

---
+++ project-management-client/src/components/projects/ProjectDetails.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import axios from "axios";
+import { Link, Redirect } from "react-router-dom";
+
+class ProjectDetails extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <div>Welcome to project details page!</div>;
+  }
+}
+
+export default ProjectDetails;
---
+++ project-management-client/src/components/projects/ProjectList.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+import AddProject from "./AddProject"; // <== !!!
+
+class ProjectList extends Component {
+  constructor() {
+    super();
+    this.state = { listOfProjects: [] };
+  }
+
+  getAllProjects = () => {
+    axios.get(`http://localhost:5000/api/projects`).then(responseFromApi => {
+      this.setState({
+        listOfProjects: responseFromApi.data
+      });
+    });
+  };
+
+  // we use componentDidMount() lifecycle method to fetch the data from API (this is very much standard practice);
+  componentDidMount() {
+    this.getAllProjects();
+  }
+
+  render() {
+    return (
+      <div>
+        <div style={{ width: "60%", float: "left" }}>
+          {/* use map() to list the projects (don’t forget to give each element the database ID as the key with key={project._id}); */}
+          {this.state.listOfProjects.map(project => {
+            return (
+              <div key={project._id}>
+                {/* we use <Link /> component from react-router-dom library to be able to dynamically change URL and in our case, to go to project’s detail page (that’s our next thing to work on); */}
+                <Link to={`/projects/${project._id}`}>
+                  <h3>{project.title}</h3>
+                </Link>
+                {/* <p style={{maxWidth: '400px'}} >{project.description} </p> */}
+              </div>
+            );
+          })}
+        </div>
+        <div style={{ width: "40%", float: "right" }}>
+          <AddProject getData={() => this.getAllProjects()} /> {/* <== !!! */}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ProjectList;
