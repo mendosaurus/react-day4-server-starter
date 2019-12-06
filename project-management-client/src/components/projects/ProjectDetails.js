8520114b429fb9fedb479e4d7876081814ce1716
mendosaurus <masaki.endo@nex.nikkei.com>
2019-12-06 17:10:57 -0500

    make edit form component and set the edit/update functionality inside details component

--- project-management-client/src/components/projects/ProjectDetails.js
+++ project-management-client/src/components/projects/ProjectDetails.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import EditProject from "./EditProject";
 
 class ProjectDetails extends Component {
   constructor(props) {
@@ -25,11 +26,29 @@ class ProjectDetails extends Component {
       });
   };
 
+  renderEditForm = () => {
+    if (!this.state.title) {
+      this.getSingleProject();
+    } else {
+      //                                                                                          ^
+      //                                                                                          |
+      //{...props} => so we can have 'this.props.history' in Edit.js
+      return (
+        <EditProject
+          theProject={this.state}
+          getTheProject={this.getSingleProject}
+          {...this.props}
+        />
+      );
+    }
+  };
+
   render() {
     return (
       <div>
         <h1>{this.state.title}</h1>
         <p>{this.state.description}</p>
+        <div>{this.renderEditForm()} </div> // !!!
         <Link to={"/projects"}>Back to projects</Link>
       </div>
     );
